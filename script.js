7171858a4de73e4e0471a05798fccfd356161fbf
Rahul Sharma <rahulsharma15@gmail.com>
2023-03-16 10:41:22 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g; //email regex
+const mobileRegex = /^[0-9]{10}$/g; //mobile regex
+const alphabetRegex = /^[A-Za-z]+$/; //only alphabet regex
+const passwordRegex = /^[a-zA-Z0-9]{8,}$/g; //password regex
